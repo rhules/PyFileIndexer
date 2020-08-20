8d37bf3e3534d7bf98a87d2c35a36b833d0b028f
Ryan Hules <38119764+rhules@users.noreply.github.com>
2020-08-19 23:46:53 -0700

    Generalize inputs for indexer and opener

--- indexer.py
+++ indexer.py
@@ -1,8 +1,20 @@
-import os
+import os, sys
 from os.path import join
-with open('..\\index', 'w', encoding='utf-8') as f:
-    #json.dump(os.walk('\\\\MYBOOKLIVE\\ryan\\Videos'), f)
-    for root, dirs, files in os.walk('\\\\MYBOOKLIVE\\ryan\\Videos'):
-        for name in dirs:
-            f.write(str(join(root, name) + "\n"))
+from typing import List
+
+index = "..\\index"
+if len(sys.argv) < 2:
+    print("too few arguments! format is \"python indexer.py [directory path]\" or \"python indexer.py [directory path] [index file]")
+elif len(sys.argv) == 2:
+    path = sys.argv[1]
+else:
+    path = sys.argv[1]
+    index = sys.argv[2]
+with open(index, 'w', encoding='utf-8') as f:
+    for root, dirs, files in os.walk(path):
+        catch: List[str] = [".mkv", ".wmv", ".mp4", ".m4v", ".mov", ".avi"]
+        for name in files:
+            for o in catch:
+                if str(name).endswith(o):
+                    f.write(str(join(root, name) + "\n"))
 f.closed
--- opener.py
+++ opener.py
@@ -2,28 +2,45 @@ import random, os
 from os.path import join
 import subprocess
 from typing import List
+import sys
 
 sets = []
-with open('..\\index', 'r', encoding='utf-8') as f:
+index = "..\\index"
+if len(sys.argv) < 2:
+    print("too few arguments! format is \"python opener.py [index file]\" or \"python opener.py [index file] [argument]")
+else:
+    index = sys.argv[1]
+with open(index, 'r', encoding='utf-8') as f:
     for line in f:
         sets.append(line.strip())
 f.closed
-arg = random.choice(sets)
-choices = []
-for root, dirs, files in os.walk(arg):
-    catch: List[str] = [".mkv", ".wmv", ".mp4", ".ogg", ".m4v"]
-    for name in files:
-        if any(o in str(name) for o in catch):
-            if " 01" in str(name):
-                choices.append(join(root, name))
-    if not choices:
-        for name in files:
-            if any(o in str(name) for o in catch):
-                choices.append(join(root, name))
-if not choices:
-    print("no valid options")
+arg = ""
+if len(sys.argv) > 2:
+    term = sys.argv[2]
+    options = []
+    for name in sets:
+        file = name.split("\\")
+        if term in file[len(file)-1]:
+            options.append(name)
+    if options:
+        arg = random.choice(options)
+    else:
+        print("no files found")
+        sys.exit()
 else:
-    arg2 = random.choice(choices)
-    print(str(arg2))
-    #subprocess.run(['open', arg2], check=True)
-    os.startfile(arg2, 'open')
+    arg = random.choice(sets)
+#for name in files:
+#    if any(o in str(name) for o in catch):
+#        if " 01" in str(name):
+#            choices.append(join(root, name))
+#if not choices:
+#    for name in files:
+#        if any(o in str(name) for o in catch):
+#            choices.append(join(root, name))
+#if not choices:
+#    print("no valid options")
+#else:
+#arg2 = random.choice(choices)
+sys.stdout.buffer.write(str(arg).encode('utf8'))
+#subprocess.run(['open', arg2], check=True)
+os.startfile(arg, 'open')
